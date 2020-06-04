9e2f7642c0e52564882995d6ae9c73f7891af6fc
emilkraft <e.kraft@protonmail.com>
2020-06-04 07:59:56 +0200

    Added regex for us currency

--- src/main/java/UsCurrencyChecker.java
+++ src/main/java/UsCurrencyChecker.java
@@ -1,9 +1,24 @@
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class UsCurrencyChecker {
     public static Matcher getMatcher(String usCurrencyString) {
-        throw new NotImplementedException();
+        /**
+         * \$ - matches a '$' character
+         * \s? - matches a optional white space
+         * \d+ - matches one or more digits
+         * (?:\.\d{1,2})?
+         *  - (?:) Non capturing group
+         *  - \. matches a '.'
+         *  - \d{1,2} - \d matches 0-9, {1-2} is a quantifier that matches between 1 or 2 of the preceding token
+         *  - ? - optional (0 or 1)
+         */
+        String pattern = "\\$\\s?\\d+(?:\\.\\d{1,2})?";
+
+        // Create a Pattern object
+        Pattern r = Pattern.compile(pattern);
+
+        // Now create matcher object.
+        return r.matcher(usCurrencyString);
     }
 }
